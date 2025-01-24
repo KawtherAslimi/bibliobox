1041f61ebedff5fb038f90782c11ecc95c482947
KawtherAslimi <40010884@parisnanterre.fr>
2025-01-24 10:51:15 +0100

    Update models.py

--- booking/models.py
+++ booking/models.py
@@ -1,7 +1,8 @@
-# booking/models.py
 from django.db import models
 from django.contrib.auth.models import User
 
+
+#Table servant a repertorier les étudiants (utile pour le principe de session)
 class StudentProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     email = models.EmailField(unique=True)
@@ -12,12 +13,14 @@ class StudentProfile(models.Model):
     def __str__(self):
         return self.email
 
+#Table servant a repertorier les noms des boxes (PIXEL A, PIXEL B, PIXEL C,...)
 class Box(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
+#Table représentant les créneaux 
 class TimeSlot(models.Model):
     box = models.ForeignKey(Box, on_delete=models.CASCADE)
     date = models.DateField(db_index=True)
@@ -27,7 +30,9 @@ class TimeSlot(models.Model):
 
     def __str__(self):
         return f"{self.box.name} - {self.date} {self.start_time}-{self.end_time}"
+    
 
+#Table servant aux reservations
 class Reservation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     time_slot = models.ForeignKey(TimeSlot, on_delete=models.CASCADE, null=True, blank=True)
@@ -39,6 +44,8 @@ class Reservation(models.Model):
             return f"{self.user.username} - {self.time_slot.box.name} - {self.time_slot.date} {self.time_slot.start_time}-{self.time_slot.end_time}"
         return f"[NoUser] - {self.time_slot.box.name} - {self.time_slot.date} {self.time_slot.start_time}-{self.time_slot.end_time}"
 
+
+# Table concernant la vérification du code 
 class VerificationCode(models.Model):
     email = models.EmailField(unique=True)
     generated_code = models.CharField(max_length=4, null=True, blank=True)
